6e7f3cbbdcd163ee7d75a314a858467ea17f54cc
yuslianto <yuslianto56@gmail.com>
2021-07-13 09:48:17 +0700

    setup project instagram fake

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Instagram Clone Apps</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
