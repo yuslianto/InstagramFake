ff523a2eaebe938a1246d0d6b453dfb37cbed513
yuslianto <yuslianto56@gmail.com>
2021-08-12 09:57:15 +0700

    Slicing page confirm email

--- src/pages/EmailTab/index.js
+++ src/pages/EmailTab/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import { View } from 'react-native';
 import { Button, Gap, TextInput } from '../../components';
 
-const EmailTab = ({navigation}) => {
+const EmailTab = ({ navigation }) => {
   return (
     <View style={{ flex: 1, width: '100%' }}>
       <Gap height={15} />
       <TextInput placeholder="your email address" />
       <Gap height={15} />
-      <Button label="Next" textColor="white" 
-      onPress={() => navigation.navigate('setNamePass')} />
+      <Button
+        label="Next"
+        textColor="white"
+        onPress={() => navigation.navigate('confirNumber')}
+      />
     </View>
-      );
+  );
 };
 
-      export {EmailTab};
+export { EmailTab };
--- src/pages/confirNumber/index.js
+++ src/pages/confirNumber/index.js
@@ -1,14 +1,50 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { Button, Gap, TextInput } from '../../components';
+
 const confirNumber = () => {
   return (
-    <View>
-      <Text>this is comfirm number page</Text>
+    <View style={styles.page}>
+      <Gap height={30} />
+      <View style={styles.wrapperText}>
+        <Text style={styles.title}>ENTER CONFIRMATION CODE</Text>
+        <Gap height={10} />
+        <Text style={styles.subTitle}>
+          Enter the confirmatiion code we send to your email.
+          <Text style={styles.textLink}>Resend code.</Text>
+        </Text>
+      </View>
+      <Gap height={20} />
+      <TextInput placeholder="Confirmation code" />
+      <Gap height={10} />
+      <Button label="Next" textColor="white" />
     </View>
   );
 };
 
 export default confirNumber;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 10,
+  },
+  wrapperText: {
+    alignItems: 'center',
+    width: '70%',
+  },
+  title: {
+    fontFamily: 'Poppins-Reguler',
+    fontSize: 20,
+  },
+  subTitle: {
+    textAlign: 'center',
+    fontFamily: 'Poppins-Reguler',
+    fontSize: 18,
+  },
+  textLink: {
+    color: '#0594f2',
+  },
+});
